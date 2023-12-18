af2a7730a42ac876121f7e0c1ac80ddfb24d639c
khmalz <akmalrma1710@gmail.com>
2023-12-18 13:51:35 +0700

    rename folder

--- src/docs/Certificates.ts
+++ src/docs/Certificates.ts
@@ -3,19 +3,19 @@ import { certificateType } from "@/types/certificateType";
 const certificatesCollection: certificateType[] = [
    {
       title: "IT Software Solution for Business - LKS Tingkat Kota Jakarta Timur 2023",
-      image: "/image/IT-Software-2023.webp",
+      image: "/images/IT-Software-2023.webp",
    },
    {
       title: "Cloud Computing Club Competiton - Regional DKI Jakarta",
-      image: "/image/Sagasitas-CloudCompetition.webp",
+      image: "/images/Sagasitas-CloudCompetition.webp",
    },
    {
       title: "Web Development Technifest",
-      image: "/image/Technifest.webp",
+      image: "/images/Technifest.webp",
    },
    {
       title: "IT Software Solution for Business - LKS Tingkat Wilayah 1 Jakarta Timur 2022",
-      image: "/image/IT-Software-2022.webp",
+      image: "/images/IT-Software-2022.webp",
    },
 ];
 
