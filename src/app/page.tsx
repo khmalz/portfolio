f9c42889381dfe96f3af8057812184e5b1ff4236
khmalz <akmalrma1710@gmail.com>
2023-12-18 10:11:48 +0700

    add certificate section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,40 @@
 "use client";
 
 import Navbar from "@/components/Navbar";
-import { useEffect, useRef } from "react";
+import { Tab } from "@headlessui/react";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 import Typed from "typed.js";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+type CertificateType = {
+   title: string;
+   image: string;
+};
+
+const certificates: CertificateType[] = [
+   {
+      title: "IT Software Solution for Business - LKS Tingkat Kota Jakarta Timur 2023",
+      image: "/image/IT-Software-2023.webp",
+   },
+   {
+      title: "Cloud Computing Club Competiton - Regional DKI Jakarta",
+      image: "/image/Sagasitas-CloudCompetition.webp",
+   },
+   {
+      title: "Web Development Technifest",
+      image: "/image/Technifest.webp",
+   },
+   {
+      title: "IT Software Solution for Business - LKS Tingkat Wilayah 1 Jakarta Timur 2022",
+      image: "/image/IT-Software-2022.webp",
+   },
+];
 
 export default function Home() {
    const typeText = useRef(null);
+   const [open, setOpen] = useState(false);
 
    useEffect(() => {
       const typed = new Typed(typeText.current, {
@@ -15,6 +44,7 @@ export default function Home() {
          backDelay: 1000,
          loop: true,
          shuffle: true,
+         cursorChar: " .",
       });
 
       return () => {
@@ -60,7 +90,7 @@ export default function Home() {
                   </p>
                   <p className="mt-5 text-justify text-sm md:text-base">
                      I started off as a front-end developer and ended up as a back-end developer because I love the challenges that are getting bigger and bigger. My favorite thing is turning source code into{" "}
-                     <span className="text-pink font-semibold">clean</span> code.
+                     <span className="font-semibold text-pink">clean</span> code.
                   </p>
                </div>
 
@@ -155,6 +185,51 @@ export default function Home() {
                         </div>
                      </div>
                   </div>
+               </div>
+            </div>
+         </section>
+
+         <section id="certificate" className="container mt-14 py-10 text-white">
+            <div className="flex flex-col">
+               <div className="flex items-center self-start">
+                  <div className="h-px w-10 flex-1 border bg-white"></div>
+                  <h4 className="mx-1 text-xl font-bold">Certificate</h4>
+               </div>
+               <div className="mt-10 flex flex-col">
+                  <Tab.Group vertical>
+                     <div className="md:flex">
+                        <Tab.List className="flex flex-col space-y-2 rounded-xl p-5 md:w-2/5 md:justify-center">
+                           {certificates.map((certificate: CertificateType, index) => (
+                              <Tab
+                                 key={index}
+                                 className={({ selected }) =>
+                                    `w-full rounded-lg py-5 text-sm font-medium leading-5 ring-white/60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2 ${
+                                       selected ? "bg-white text-blue-700 shadow" : "bg-fourth text-white hover:bg-white/[0.12] hover:text-white"
+                                    }`
+                                 }>
+                                 {certificate.title}
+                              </Tab>
+                           ))}
+                        </Tab.List>
+                        <Tab.Panels className="items-center justify-center p-5 text-black md:flex md:w-3/5">
+                           {certificates.map((certificate: CertificateType, index) => (
+                              <Tab.Panel key={index} className="w-full rounded-lg bg-white p-4 md:w-[580px]">
+                                 <Image width={500} height={400} className="w-full cursor-pointer rounded-lg border object-cover shadow-md" src={certificate.image} alt={certificate.title} onClick={() => setOpen(true)} />
+                                 <Lightbox
+                                    styles={{ container: { backgroundColor: "rgba(0, 0, 0, .85)" } }}
+                                    render={{
+                                       buttonPrev: () => null,
+                                       buttonNext: () => null,
+                                    }}
+                                    open={open}
+                                    close={() => setOpen(false)}
+                                    slides={[{ src: certificate.image, alt: certificate.title }]}
+                                 />
+                              </Tab.Panel>
+                           ))}
+                        </Tab.Panels>
+                     </div>
+                  </Tab.Group>
                </div>
             </div>
          </section>
