d9a467e52c235ee897aed967538721c657ed0547
khmalz <akmalrma1710@gmail.com>
2023-12-21 01:06:14 +0700

    gives lang-based conditions for cv download

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SvgTech from "./SvgTech";
 import { svgTechType } from "@/types/svgTechType";
 import techsCollection from "@/docs/techs";
 import { useTranslations } from "next-intl";
+import { usePathname } from "next/navigation";
 
 export default function AboutSection() {
    const trlns = useTranslations("about");
    const [techs] = useState<svgTechType[]>(techsCollection);
+   const [lang, setLang] = useState<string>("");
+
+   useEffect(() => {
+      const langValue = document.documentElement.lang;
+      setLang(() => langValue);
+   }, []);
 
    return (
       <section id="about" className="container pt-16">
@@ -23,13 +30,14 @@ export default function AboutSection() {
                </p>
                <a
                   target="_blank"
-                  href="/CV-Khairul_Akmal.pdf"
+                  href={`/CV-${lang}-Khairul_Akmal.pdf`}
+                  download
                   className="inline-flex items-center self-baseline rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-fourth focus:z-10 focus:text-fourth focus:outline-none focus:ring-4 focus:ring-gray-200">
                   <svg className="me-2.5 h-3.5 w-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                      <path d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z" />
                      <path d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z" />
                   </svg>
-                  Check CV
+                  Download CV
                </a>
             </div>
 
