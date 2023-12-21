14333709452ed811d344283c4a01d7b0cdf97001
khmalz <akmalrma1710@gmail.com>
2023-12-21 08:00:02 +0700

    change gmail href in contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -21,7 +21,7 @@ export default function ContactSection() {
                   }}
                />
                <div className="flex flex-col items-baseline">
-                  <a target="_blank" href="mailto:lirkmal171@gmai.com" className="my-2 flex items-center space-x-2 rounded-lg bg-gmail px-3 py-2 text-center text-xs text-white hover:bg-opacity-90">
+                  <a target="_blank" href="mailto:akmalrma1710@gmail.com" className="my-2 flex items-center space-x-2 rounded-lg bg-gmail px-3 py-2 text-center text-xs text-white hover:bg-opacity-90">
                      <svg className="h-4 w-4" fill="currentColor" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <title>Gmail</title>
                         <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z" />
