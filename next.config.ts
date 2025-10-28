cc231df57d77c3ae72321536f58502700d5a0e98
khmalz <akmalrma1710@gmail.com>
2025-10-28 16:07:09 +0700

    add turbopack

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 export const nextConfig: NextConfig = {
    trailingSlash: true,
+   experimental: {
+      turbo: {},
+   },
 };
 
 const withNextIntl = createNextIntlPlugin();
