890b0f3ac7c9f8abfb48966aae2aaab72acdcd9c
khmalz <akmalrma1710@gmail.com>
2025-10-28 14:09:34 +0700

    update next-intl version to latest

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+
+export const nextConfig: NextConfig = {
+   trailingSlash: true,
+};
+
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(nextConfig);
