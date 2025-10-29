bb70560c07ef631d11db0a3a97ea2aa4ed189824
khmalz <akmalrma1710@gmail.com>
2025-10-29 23:42:13 +0700

    remove cv from asset and move to drive link

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -10,6 +10,14 @@ export default function AboutSection() {
 
    const techs: svgTechType[] = techsCollection;
 
+   const isId = lang === "id";
+   const cv = isId ? process.env.CV_ID : process.env.CV_EN;
+   if (!cv) {
+      console.warn(`CV_ID atau CV_EN belum di-set di .env`);
+   }
+
+   const cvUrl = `https://drive.google.com/file/d/${cv}/view`;
+
    return (
       <section id="about" className="container pt-16">
          <div className="bg-secondary flex flex-col justify-between space-y-8 rounded-xl px-5 py-10 text-white shadow-xl md:flex-row md:space-y-0 md:space-x-8">
@@ -25,14 +33,14 @@ export default function AboutSection() {
                </p>
                <a
                   target="_blank"
-                  href={`/CV-${lang}-Khairul_Akmal.pdf`}
+                  href={cvUrl}
                   download
                   className="hover:text-fourth focus:text-fourth inline-flex items-center self-baseline rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:z-10 focus:ring-4 focus:ring-gray-200 focus:outline-hidden">
                   <svg className="me-2.5 h-3.5 w-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                      <path d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z" />
                      <path d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z" />
                   </svg>
-                  Download CV
+                  Preview CV
                </a>
             </div>
 
